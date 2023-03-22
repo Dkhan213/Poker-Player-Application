45355c96274a9236925350991b03675483b1857a
Daniel Khan <dkhan321@gmail.com>
2023-03-21 22:54:55 -0500

    Added notations

--- src/com/ppa/poker/HandGenerator.java
+++ src/com/ppa/poker/HandGenerator.java
@@ -4,89 +4,90 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HandGenerator {
-    public static List<Hand> generateHands() {
-        List<Hand> hands = new ArrayList<>();
+    // This class creates a randomly generated hand to be used by the hand analyzer.
+    public static List<booleans.poker.Hand> generateHands() {
+        List<booleans.poker.Hand> hands = new ArrayList<>();
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.ACE, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.KING, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.QUEEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.ACE, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.KING, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.QUEEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.FIVE, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.QUEEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TWO, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FIVE, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.QUEEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TWO, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.CLUBS),
-                new Card(Card.Rank.TEN, Card.Suit.SPADES),
-                new Card(Card.Rank.TEN, Card.Suit.HEARTS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.CLUBS),
-                new Card(Card.Rank.THREE, Card.Suit.SPADES),
-                new Card(Card.Rank.JACK, Card.Suit.HEARTS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.CLUBS),
-                new Card(Card.Rank.THREE, Card.Suit.SPADES),
-                new Card(Card.Rank.THREE, Card.Suit.HEARTS),
-                new Card(Card.Rank.THREE, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.CLUBS),
-                new Card(Card.Rank.FIVE, Card.Suit.SPADES),
-                new Card(Card.Rank.THREE, Card.Suit.HEARTS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FIVE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.KING, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TEN, Card.Suit.CLUBS),
-                new Card(Card.Rank.FIVE, Card.Suit.SPADES),
-                new Card(Card.Rank.THREE, Card.Suit.HEARTS),
-                new Card(Card.Rank.JACK, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.KING, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FIVE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.JACK, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.THREE, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TWO, Card.Suit.CLUBS),
-                new Card(Card.Rank.FIVE, Card.Suit.SPADES),
-                new Card(Card.Rank.SIX, Card.Suit.HEARTS),
-                new Card(Card.Rank.FOUR, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TWO, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FIVE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.SIX, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FOUR, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.THREE, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.TWO, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.FIVE, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.SIX, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.FOUR, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.TWO, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FIVE, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.SIX, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FOUR, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
-        hands.add(new Hand(new Card[] {
-                new Card(Card.Rank.TEN, Card.Suit.DIAMONDS),
-                new Card(Card.Rank.FOUR, Card.Suit.CLUBS),
-                new Card(Card.Rank.THREE, Card.Suit.SPADES),
-                new Card(Card.Rank.THREE, Card.Suit.HEARTS),
-                new Card(Card.Rank.THREE, Card.Suit.DIAMONDS),
+        hands.add(new booleans.poker.Hand(new booleans.poker.Card[] {
+                new booleans.poker.Card(booleans.poker.Card.Rank.TEN, booleans.poker.Card.Suit.DIAMONDS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.FOUR, booleans.poker.Card.Suit.CLUBS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.SPADES),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.HEARTS),
+                new booleans.poker.Card(booleans.poker.Card.Rank.THREE, booleans.poker.Card.Suit.DIAMONDS),
         }));
 
         return hands;
